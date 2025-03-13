4b57566ed900667ab03d4af38a402aa8959aaf6b
Drew Krehel <drewkrehel@gmail.com>
2025-03-13 00:25:52 +0000

    chat dev start 1

--- chat.rb
+++ chat.rb
@@ -3,7 +3,7 @@ require "dotenv/load"
 
 client = OpenAI::Client.new(access_token: ENV.fetch("OPEN_AI_KEY"))
 
-pp client
+#pp client
 
 # Prepare an Array of previous messages
 message_list = [
@@ -25,4 +25,14 @@ api_response = client.chat(
   }
 )
 
-pp api_response
+#pp api_response
+parsed_choice = api_response.fetch("choices")
+choice_result = parsed_choice.at(0)
+parsed_message = choice_result.fetch("message")
+first_message = parsed_message.fetch("content")
+
+#Program Start
+puts "Hello! How can I help you today?"
+puts 50*"-"
+user_input = gets.chomp
+message_list << user_input
